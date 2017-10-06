c8d761dfd843a203b65d4036d7ed561ddd7e5454
Arthur Freiberger <arthur@careerhub.com.au>
2017-10-06 15:13:01 +1000

    few real testimonials

--- user/themes/refuge-theme/js/main.js
+++ user/themes/refuge-theme/js/main.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
 	if (typedDiv3.length) {
 		
 		var testimonials = {
-		strings: [, "<div class='text'>Refuge is simply THE GAME. Play it, PLAY IT! It is AWESOME!</div><div class='author'>Jason Ogro @ Itch.io</div>", "<div class='text'>One of the best games I had the chance to play. Nice job!</div><div class='author'>Silly Fella @ Steam</div>" ],
+		strings: [, "<div class='text'>Refuge is a hell of an immersive game! The fact it tackles real world issues creates even more empathy towards the game and its story. Loved it!</div><div class='author'>Jason @ SAE</div>", "<div class='text'>These devs are definitely passionate about what they do, and this game reflects their passion. Refuge was so well built, and taken care of, it was impossible for the game not to be great. Great job guys, I loved playing it, and highly recommend this to everyone out there.</div><div class='author'>Arthur @ SAE Qantm</div>" ],
 		  loop: true,
 		  typeSpeed: 35,
 		  backSpeed: 6,
